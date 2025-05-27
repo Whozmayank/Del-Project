034e47c356d288334351eee5e3fda87c3f925d35
Whozmayank <mayanktomar878@gmail.com>
2025-05-27 20:14:27 +0530

    Issue Updated

--- app.js
+++ app.js
@@ -80,6 +80,8 @@ app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 
 app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
     res.locals.currUser = req.user;
     next();
 });
