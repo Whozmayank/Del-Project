fe4586d3b5549b5154566099aea7471c89740d45
Whozmayank <mayanktomar878@gmail.com>
2025-05-27 21:38:54 +0530

    Issue solved

--- app.js
+++ app.js
@@ -17,14 +17,6 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;   
 const User = require("./models/user.js");
 
-
-app.use((req, res, next) => {
-    res.locals.success = req.flash("success");
-    res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
-    next();
-});
-
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
@@ -91,7 +83,12 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.locals.currUser = req.user;
+    next();
+});
 
 
 app.use("/listings", listingRouter);
